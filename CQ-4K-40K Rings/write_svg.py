d80cb3db03f1d3ea85ad733145c74faa86646283
morgensternR <49378275+morgensternR@users.noreply.github.com>
2021-07-30 13:01:03 -0600

    Rings Update
    Removed Folder
    Fixed Ring.py and added their Steps to output

--- CQ-4K-40K Rings/write_svg.py
+++
@@ -1,6 +0,0 @@
-def write_svg(part, name, direction):
-    with open(f'./svg/{name}','w') as f:
-        f.write(part.toSvg(opts={"projectionDir": direction,
-                              "showHidden": False,
-                              "height": 800,
-                              "width": 800,}))
