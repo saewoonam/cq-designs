000ced29c711d59ef7b57474374877a79e6516ff
morgensternR <49378275+morgensternR@users.noreply.github.com>
2021-08-06 10:47:41 -0600

    Blank Mod
    Modified Blank.py to include a KF50 Port.
    Added hole callout.
    Added KF50 bulk head clamp STEP.

--- ISO320_blankmod/blank_KF50.py
+++ ISO320_blankmod/blank_KF50.py
@@ -13,7 +13,7 @@ import uuid
 def fixpath(path):
     return os.path.abspath(os.path.expanduser(path))
 inch = 25.4
-
+dist = -30
 if True:
     s = cq.importers.importStep('./qf320-bk.stp')
     bbox = s.objects[0].BoundingBox()
@@ -22,16 +22,16 @@ if True:
     #s = s.workplaneFromTagged("top")
     
     s = (s.workplaneFromTagged("top")
-         #.moveTo(-80,0)
+         .moveTo(dist,0)
          .hole(5*inch)  # through hole
          .faces(">Z")
          .workplane()
-         #.moveTo(-80,0)
+         .moveTo(dist,0)
          .polygon(6,6.5*inch,forConstruction=True)  # mounting screws
          .vertices().hole(*tap('M6', 3.0*inch/8.0))
          .faces(">Z")
          .workplane()
-         #.moveTo(-80,0)
+         .moveTo(dist,0)
         )
     s = oring('2-254', s)
 
@@ -43,20 +43,26 @@ else:
          )
 
     
-"""
+
 # 1/4-20 mounting holes
 s = (s.workplaneFromTagged("top")
-     .rect(10*inch, 10*inch, forConstruction=True)
+     .rect(8*inch, 10*inch, forConstruction=True)
      .vertices()
      .hole(*tap('1/4-20', 0.375*inch))
      )
-"""
-for angle in range(-1, 2):
-    s = s.workplaneFromTagged(f"top").transformed(rotate=(0,0,angle*45)).center(130,0)
-    s = nw_bulkhead(s, 'nw40', 20, offset=True) #, offset=True)
+
+for angle in (-1, 1):
+    s = s.workplaneFromTagged(f"top").transformed(rotate=(0,0,angle*90)).center(130,0)
+    s = nw_bulkhead(s, 'nw40', 20, offset=True)#, offset=True)
+    
+s = s.workplaneFromTagged(f"top").center(120,0)
+s = nw_bulkhead(s, 'nw50', 20, offset=True)
 
 clamp = cq.importers.importStep('./qf40-150-bc.stp')
-clamp= clamp.rotate((0,0,0), (0,1,0), 90).translate((130,0,20))
+clamp= clamp.rotate((0,0,0), (0,1,0), 90).translate((0,130,20))
+
+clamp50 = cq.importers.importStep('./qf50-200-bc.stp')
+clamp50 = clamp50.rotate((0,0,0), (0,1,0), 90).translate((120,0,20))
 
 pt405_cover = cq.Workplane("XY").circle(3.5*inch).extrude(6)
 pt405_cover = ( pt405_cover.faces(">Z")
@@ -64,7 +70,9 @@ pt405_cover = ( pt405_cover.faces(">Z")
          .polygon(6,6.5*inch,forConstruction=True)  # mounting screws
          .vertices().hole(*thru('M6', 3.0*inch/8.0))
          )
-pt405_cover = pt405_cover.translate((0, 0, bbox.zmax))
+pt405_cover = pt405_cover.translate((dist, 0, bbox.zmax))
 show_object(s)
 show_object(pt405_cover)
 show_object(clamp)
+show_object(clamp50)
+cq.exporters.export(s, '. /Iso320BlankMod.step')
